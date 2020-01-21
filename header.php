680a2ce551712c56dbd9fec3d725e7a3fe6cf3d0
Isabella Chen <miss.izzyc@gmail.com>
2020-01-21 17:29:54 +0100

    style header for mobile

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,47 +13,59 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
+
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'madhouse-heaven' ); ?></a>
+  <div id="page" class="site">
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'madhouse-heaven'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$madhouse_heaven_description = get_bloginfo( 'description', 'display' );
-			if ( $madhouse_heaven_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $madhouse_heaven_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <header id="masthead" class="site_header">
+      <div class="site-branding">
+        <?php
+        the_custom_logo();
+        ?>
+      </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'madhouse-heaven' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+      <nav id="site-navigation" class="site_header-navigation">
+        <div class="primary-menu-wrapper">
+          <div class="primary-menu-title">DESTINATIONS</div>
+          <div class="primary-menu-title--mobile"><img src="<?php echo get_template_directory_uri(); ?>/assets/earth.svg"></div>
+          <?php
+          wp_nav_menu(array(
+            'theme_location' => 'primary',
+            'menu_id'        => 'primary-menu',
+            'container_class' => 'primary-menu-container',
+            'menu_class' => 'primary-menu',
+          ));
+          ?>
+        </div>
+        <div class="secondary-menu-wrapper">
+          <div class="secondary-menu-title">
+            <button class="hamburger hamburger--squeeze" type="button">
+              <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+              </span>
+            </button>
+          </div>
+          <?php
+          wp_nav_menu(array(
+            'theme_location' => 'secondary',
+            'menu_id'        => 'secondary-menu',
+            'container_class' => 'secondary-menu-container',
+            'menu_class' => 'secondary-menu'
+          ));
+          ?>
+        </div>
+        <?php
+        ?>
+      </nav><!-- #site-navigation -->
+    </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
